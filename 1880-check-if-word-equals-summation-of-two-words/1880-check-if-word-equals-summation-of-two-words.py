8a9873136900a1f15abeaaf15609339c52274f6e
Prabal Jay <2k23.cs2313758@gmail.com>
2026-07-11 16:56:09 +0530

    Time: 0 ms (100%), Space: 19.3 MB (28.46%) - LeetHub

---
+++ 1880-check-if-word-equals-summation-of-two-words/1880-check-if-word-equals-summation-of-two-words.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isSumEqual(self, firstWord: str, secondWord: str, targetWord: str) -> bool:
+        def convert(word):
+            num=""
+            for ch in word:
+                num+=str(ord(ch)-ord('a'))
+            return int(num)
+        return convert(firstWord)+convert(secondWord)==convert(targetWord)
