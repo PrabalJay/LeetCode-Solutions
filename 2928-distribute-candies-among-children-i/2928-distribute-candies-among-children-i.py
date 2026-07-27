80f2ac84495a8c6a0377c818bb8ab310c26bc7a0
Prabal Jay <2k23.cs2313758@gmail.com>
2026-07-27 23:06:13 +0530

    Time: 22 ms (40.27%), Space: 19.3 MB (62.9%) - LeetHub

---
+++ 2928-distribute-candies-among-children-i/2928-distribute-candies-among-children-i.py
@@ -0,0 +1,17 @@
+class Solution:
+    def distributeCandies(self, n: int, limit: int) -> int:
+        # ans=0
+        # for i in range(limit+1):
+        #     for j in range(limit+1):
+        #         for k in range(limit+1):
+        #             if i+j+k==n:
+        #                 ans+=1
+        # return ans
+
+        ans=0
+        for i in range(limit+1):
+            for j in range(limit+1):
+                k=n-i-j
+                if 0<=k<=limit:
+                    ans+=1
+        return ans
