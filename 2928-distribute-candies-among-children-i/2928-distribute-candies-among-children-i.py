3e684b4780e14ed7536e560aeed0af1637d2b63c
Prabal Jay <2k23.cs2313758@gmail.com>
2026-07-27 23:09:17 +0530

    Time: 3 ms (83.26%), Space: 19.2 MB (62.9%) - LeetHub

--- 2928-distribute-candies-among-children-i/2928-distribute-candies-among-children-i.py
+++ 2928-distribute-candies-among-children-i/2928-distribute-candies-among-children-i.py
@@ -8,10 +8,20 @@ class Solution:
         #                 ans+=1
         # return ans
 
+# --------------------------------------------------
+        # ans=0
+        # for i in range(limit+1):
+        #     for j in range(limit+1):
+        #         k=n-i-j
+        #         if 0<=k<=limit:
+        #             ans+=1
+        # return ans
+
+# ----------------------------------------------------
         ans=0
         for i in range(limit+1):
-            for j in range(limit+1):
-                k=n-i-j
-                if 0<=k<=limit:
-                    ans+=1
+            left=max(0,n-i-limit)
+            right=min(limit,n-i)
+            if left<=right:
+                ans+=right-left+1
         return ans
